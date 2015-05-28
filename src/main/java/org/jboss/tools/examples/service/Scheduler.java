d9b5161df84f4c34763a062c9f3fbaf39b2ece2a
Andreas Egger <egger.andreas.1@gmail.com>
2015-05-28 20:49:29 +0200

    Import sample webapp project

---
+++ src/main/java/org/jboss/tools/examples/service/Scheduler.java
@@ -0,0 +1,15 @@
+package org.jboss.tools.examples.service;
+
+import java.util.Date;
+
+import javax.ejb.Schedule;
+import javax.ejb.Stateless;
+
+@Stateless
+public class Scheduler {
+
+	@Schedule(second="0", minute="0", hour="*")
+	public void importData() {
+		System.out.println(new Date());
+	}
+}
